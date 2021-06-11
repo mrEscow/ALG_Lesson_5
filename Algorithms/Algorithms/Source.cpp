cf0153ae5a9b252308ebcb41b14d390c585956d2
mr.Escow <escowxbox@gmail.com>
2021-06-11 21:06:37 +0300

    26

--- Algorithms/Algorithms/Source.cpp
+++ Algorithms/Algorithms/Source.cpp
@@ -1,4 +1,5 @@
 #include <string>
+#include <windows.h>
 #include <cstring>  // for wcscpy_s, wcscat_s
 #include <cstdlib>  // for _countof
 #include <iostream> // for cout, includes <cstdlib>, <cstring>
@@ -7,6 +8,7 @@
 using std::cout;
 using std::endl;
 using std::cin;
+using std::string;
 
 #include "Header.h"
 #include "Source.h"
@@ -33,8 +35,102 @@ void password_caesar(std::string& str, int key) {
 }
 
 // 2. Реализовать шифрование и расшифровку перестановками с передаваемым в функцию сообщением и количеством столбцов
+string coder(string s, int w)
+{
+	int h;
+
+	if (s.length() % w != 0) 
+		h = s.length() / w + 1;
+	else
+		h = s.length() / w;
+
+	//char a[h][w] ;
+
+	/*выделяем количество строк в массиве*/
+	char** a = new char* [h];
+
+	/*выделяем количество столбцов в массиве*/
+	for (int i = 0; i < w; i++) {
+		a[i] = new char[w];
+	}
+
+
+		int k = 0;
+
+	for (int i = 0; i < h; i++)
+	{
+		for (int j = 0; j < w; j++)
+		{
+			if (k < s.length())
+			{
+				a[i][j] = s[k];
+				k++;
+			}
+			else a[i][j] = ' ';
+		}
+	}
+
+	string code;
+
+	for (int i = 0; i < w; i++)
+	{
+		for (int j = 0; j < h; j++)
+		{
+			code += a[j][i];
+		}
+	}
+
+	for (int i = 0; i < w; i++) {
+
+		delete[] a[i];
+	}
+
+	//delete[] a;
+
+
+	return code;
+}
+string decoder(string s, int w)
+{
+	int h = s.length() / w;;
+
+	//char a[w][h];
+	/*выделяем количество строк в массиве*/
+	char** a = new char* [h];
+	/*выделяем количество столбцов в массиве*/
+	for (int i = 0; i < w; i++) {
+		a[i] = new char[w];
+	}
+
+	int k = 0;
+	for (int i = 0; i < w; i++)
+	{
+		for (int j = 0; j < h; j++)
+		{
+			a[i][j] = s[k];
+			k++;
+		}
+	}
+
+	string decode;
+	for (int i = 0; i < h; i++)
+	{
+		for (int j = 0; j < w; j++)
+		{
+			decode += a[j][i];
+		}
+	}
+
+	for (int i = 0; i < w; i++) {
+		delete[] a[i];
+	}
+	//delete[] a;
+
+	return decode;
+}
 
 int main() {
+	setlocale(LC_ALL, "Russian");
 
 	//std::string str = "Hello World";
 	//int key; 
@@ -46,6 +142,14 @@ int main() {
 	//key = -4;
 	//password_caesar(str, key);
 	//cout << "STRING: " << str << "  KEY: " << key << endl;
+
+	string s;
+	cout << "Кодируемое слово:" << endl;
+	getline(cin, s);
+	int w;
+	cout << "Ключ (ширина таблицы):" << endl;
+	cin >> w;
+	cout << "Закодированная фраза : " << coder(s, w) << endl << "Декодированная фраза : " << decoder(coder(s, w), w);
 
 	return 0;
 }
