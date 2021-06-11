3c4c248f711ced0b9361dec7cfdc13c4b241bd7e
mr.Escow <escowxbox@gmail.com>
2021-06-11 17:24:50 +0300

    22

--- Algorithms/Algorithms/Source.cpp
+++ Algorithms/Algorithms/Source.cpp
@@ -37,12 +37,47 @@ int hashFunction(std::string String)  {
 }
 
 
+//2. Имеются монеты номиналом 50, 10, 5, 2, 1 коп. 
+//   Напишите функцию которая минимальным количеством монет наберет сумму 98 коп. 
+//   Для решения задачи используйте “жадный” алгоритм.
 
+int countCoins(int n) {
 
+    int count = 0;
+    int i = 0;
+
+    count += n / 50;
+    n -= (count-i) * 50;
+    i = count;
+
+    count += n / 10;
+    n -= (count - i) * 10;
+    i = count;
+
+    count += n / 5;
+    n -= (count - i) * 5;
+    i = count;
+
+    count += n / 2;
+    n -= (count - i) * 2;
+    i = count;
+
+    count += n / 1;
+    n -= (count - i) * 1;
+    i = count;
+
+    return count;
+}
 
 int main() {
 
-    std::string String = "ABC";
-    cout << hashFunction(String) << endl;
+    //std::string String = "ABC";
+    //cout << hashFunction(String) << endl;
+
+    int arr[5] = { 50,10,5,2,1 };
+    int n = 100;
+    //cout << n / 50 << endl;
+    cout << countCoins(n);
+
 	return 0;
 }
